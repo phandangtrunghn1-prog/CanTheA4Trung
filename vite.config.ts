6953df9661fa4bdbe4a6dc6695175524e036013b
Phan Dang Trung <phandangtrungtv2@gmail.com>
2026-09-16 19:40:26 +0700

    Build and deploy Vite app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === 'true' ? '/CanTheA4Trung/' : '/',
   plugins: [react()],
   worker: { format: 'iife' },
   build: { target: 'es2022', chunkSizeWarningLimit: 700 },
